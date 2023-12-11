7f0a7271e5dc0edb7ec7c5fc40b8f383b200ad4b
Thiago Felipe <thiagofelipefluz3@gmail.com>
2023-12-11 17:58:25 -0300

    Aula 06

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Pega o elemento pelo nome da classe */
+const hamburguer = document.querySelector('.hamburguer')
+/* pega a barra de navegação */
+const nav = document.querySelector(".nav")
+
+/* adiciona o evento onclick ao botão hamburguer */
+hamburguer.addEventListener('click', () => { /* função arrow */
+    nav.classList.toggle("active")
+    console.log(nav.classList.value)
+})
+
+
